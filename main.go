7fce3a393ab6679d309c4d398dee4f00689adc28
Rico Metzger <rico.metzger@gmail.com>
2025-03-31 23:47:06 +0300

    contact page with old routing

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:rico.metzger@gmail.com\">rico.metzger@gmail.com</a>.</p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("GET /", homeHandler)
+	http.HandleFunc("GET /contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
