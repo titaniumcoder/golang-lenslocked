253c14e1fe0591a30355cf9bed07acf6dc228d30
Rico Metzger <rico.metzger@gmail.com>
2025-04-01 10:56:11 +0300

    Using a router

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import (
 	"net/http"
 )
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
@@ -16,8 +29,11 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("GET /", homeHandler)
-	http.HandleFunc("GET /contact", contactHandler)
+	var router Router
+	/*
+		http.HandleFunc("GET /", homeHandler)
+		http.HandleFunc("GET /contact", contactHandler)
+	*/
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
