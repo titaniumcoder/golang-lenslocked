0563952b6268577d3beef030511329f088d928e5
Rico Metzger <rico.metzger@gmail.com>
2025-04-02 00:45:23 +0300

    all templates

--- main.go
+++ main.go
@@ -12,48 +12,32 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "home.gohtml"))
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "contact.gohtml")
+)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		log.Printf("Error parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		log.Printf("processing template: %v", err)
+		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
 		return
 	}
-
 	err = tpl.Execute(w, nil)
-
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
 		return
 	}
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:rico.metzger@gmail.com\">rico.metzger@gmail.com</a>.</p>")
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul>
-		<li>
-			<b>Is there a free version?</b>
-			Yes! We offer a free trial for 30 days on any paid plans.
-		</li>
-		<li>
-			<b>What are your support hours?</b>
-			We have support staff answering emails 24/7, though response
-			times may be a bit slower on weekends.
-		</li>
-		<li>
-			<b>How do I contact support?</b>
-			Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-		</li>
-	</ul>
-	`)
 }
 
 func main() {
