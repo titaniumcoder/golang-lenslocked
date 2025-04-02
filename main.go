63a0dcb178b153f157f8acca562b84f6cb2c3913
Rico Metzger <rico.metzger@gmail.com>
2025-04-02 19:23:40 +0300

    template approach

--- main.go
+++ main.go
@@ -14,13 +14,13 @@ import (
 func main() {
 	r := http.NewServeMux()
 
-	homeTpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
-	contactTpl := views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
-	faqTpl := views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
+	homeTpl := views.Must(views.ParseFS(templates.FS, "layout-page.html", "home-page.html"))
+	contactTpl := views.Must(views.ParseFS(templates.FS, "layout-page.html", "contact-page.html"))
+	faqTpl := views.Must(views.ParseFS(templates.FS, "layout-page.html", "faq-page.html"))
 
 	r.HandleFunc("GET /", controllers.StaticHandler(homeTpl))
 	r.HandleFunc("GET /contact", controllers.StaticHandler(contactTpl))
-	r.HandleFunc("GET /faq", controllers.StaticHandler(faqTpl))
+	r.HandleFunc("GET /faq", controllers.FAQ(faqTpl))
 
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
--- views/template.go
+++ views/template.go
@@ -18,8 +18,8 @@ func Parse(filepath string) (Template, error) {
 	}, nil
 }
 
-func ParseFS(fs fs.FS, pattern string) (Template, error) {
-	htmlTpl, err := template.ParseFS(fs, pattern)
+func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
+	htmlTpl, err := template.ParseFS(fs, pattern...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
