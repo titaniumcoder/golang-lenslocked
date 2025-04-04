2b6104772d8580b06e2744d4e1a9d1fc0f9c44a2
Rico Metzger <rico.metzger@gmail.com>
2025-04-04 11:00:06 +0300

    adding environment variables

--- controllers/static.go
+++ controllers/static.go
@@ -7,6 +7,14 @@ import (
 	"github.com/titaniumcoder/golang-lenslocked/views"
 )
 
+type Static struct {
+	Template Template
+}
+
+func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	static.Template.Execute(w, nil)
+}
+
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 
 	"github.com/titaniumcoder/golang-lenslocked/controllers"
 	"github.com/titaniumcoder/golang-lenslocked/templates"
@@ -10,16 +14,36 @@ import (
 )
 
 func main() {
+	fmt.Println(os.Getenv("DATABASE_URL"))
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to the database!")
+	defer db.Close()
 	r := http.NewServeMux()
 
 	r.HandleFunc("GET /", controllers.StaticHandler(parsePage("home-page.html")))
 	r.HandleFunc("GET /contact", controllers.StaticHandler(parsePage("contact-page.html")))
 	r.HandleFunc("GET /faq", controllers.FAQ(parsePage("faq-page.html")))
 
+	var usersC controllers.Users
+	usersC.Templates.New = parsePage("users/new.html")
+	r.HandleFunc("GET /users/new", usersC.New)
+	r.HandleFunc("POST /users", usersC.Create)
+
 	r.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.FS(templates.StaticFS))))
 
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	http.ListenAndServe(":"+port, r)
 }
 
 func parsePage(name string) views.Template {
